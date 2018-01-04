006bfc39a9d74ea6dae621a3113086dc833f6141
FreneticFeline <freneticfeline@users.noreply.github.com>
2018-01-04 09:27:25 -0500

    Typo

--- src/main/java/net/unladenswallow/minecraft/autofish/ModAutoFish.java
+++ src/main/java/net/unladenswallow/minecraft/autofish/ModAutoFish.java
@@ -42,7 +42,7 @@ public class ModAutoFish {
             "Enable MultiRod",
             "Enable Break Protection",
             "Re-Cast Delay",
-            "Enable Enity Clear Protection",
+            "Enable Entity Clear Protection",
             "Enable Aggressive Bite Detection",
             "Handle Problems",
             "Enable Fast Fishing"
@@ -61,6 +61,7 @@ public class ModAutoFish {
         ModAutoFish.proxy.preInit(preInitEvent);
         configFile = new Configuration(preInitEvent.getSuggestedConfigurationFile());
         configFile.renameProperty(Configuration.CATEGORY_GENERAL, "Fast Fishing", "Enable Fast Fishing");
+        configFile.renameProperty(Configuration.CATEGORY_GENERAL, "Enable Enity Clear Protection", "Enable Entity Clear Protection");
         configFile.setCategoryPropertyOrder(Configuration.CATEGORY_GENERAL, propertyOrder);
         syncConfig();
     }
@@ -75,7 +76,7 @@ public class ModAutoFish {
         config_autofish_enable = configFile.getBoolean("Enable AutoFish", Configuration.CATEGORY_GENERAL, CONFIG_DEFAULT_AUTOFISH_ENABLE, "Automatically reel in and re-cast when a fish nibbles the hook. If set to false, all other AutoFish functionality is disabled.");
         config_autofish_multirod = configFile.getBoolean("Enable MultiRod", Configuration.CATEGORY_GENERAL, CONFIG_DEFAULT_AUTOFISH_MULTIROD, "Automatically switch to a new fishing rod when the current rod breaks, if one is available in the hotbar.");
         config_autofish_preventBreak = configFile.getBoolean("Enable Break Protection", Configuration.CATEGORY_GENERAL, CONFIG_DEFAULT_AUTOFISH_PREVENTBREAK, "Stop fishing or switch to a new rod just before the current rod breaks.  Useful if you want to repair your enchanted fishing rods.");
-        config_autofish_entityClearProtect = configFile.getBoolean("Enable Enity Clear Protection", Configuration.CATEGORY_GENERAL, CONFIG_DEFAULT_AUTOFISH_ENTITYCLEARPROTECT, "When playing on a server, re-cast after the server clears entities.  Useful if you are playing on a server that periodically deletes all entities (including fishing hooks) from the world, which causes you to stop fishing.");
+        config_autofish_entityClearProtect = configFile.getBoolean("Enable Entity Clear Protection", Configuration.CATEGORY_GENERAL, CONFIG_DEFAULT_AUTOFISH_ENTITYCLEARPROTECT, "When playing on a server, re-cast after the server clears entities.  Useful if you are playing on a server that periodically deletes all entities (including fishing hooks) from the world, which causes you to stop fishing.");
         config_autofish_recastDelay = configFile.getInt("Re-Cast Delay", Configuration.CATEGORY_GENERAL, CONFIG_DEFAULT_AUTOFISH_RECASTDELAY, 1, 10, "Time (in seconds) to wait before automatically re-casting. Increase this value if server lag causes re-casting to fail.");
         config_autofish_fastFishing = configFile.getBoolean("Enable Fast Fishing", Configuration.CATEGORY_GENERAL, CONFIG_DEFAULT_AUTOFISH_FASTFISHING, "[SINGLE PLAYER ONLY] Fish will bite right after casting.");
         config_autofish_aggressiveBiteDetection = configFile.getBoolean("Enable Aggressive Bite Detection", Configuration.CATEGORY_GENERAL, CONFIG_DEFAULT_AUTOFISH_AGGRESSIVEBITEDETECTION, "When playing on a server, be more aggressive about detecting fish bites.  Improves multiplayer bite detection from ~85% to ~95%, but false positives will be more likely, especially if other players are fishing very close by.");
