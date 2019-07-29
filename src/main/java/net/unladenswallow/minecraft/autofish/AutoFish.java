87a302556bc1530fcad94964f9c6ce79af1abc08
FreneticFeline <freneticfeline@users.noreply.github.com>
2019-07-29 17:30:06 -0400

    Update for Forge 1.14.4

--- src/main/java/net/unladenswallow/minecraft/autofish/AutoFish.java
+++ src/main/java/net/unladenswallow/minecraft/autofish/AutoFish.java
@@ -656,10 +656,10 @@ public class AutoFish {
     }
 
     private ServerPlayerEntity getServerPlayerEntity() {
-        if (this.minecraftClient.getIntegratedServer() == null || this.minecraftClient.getIntegratedServer().getWorld(DimensionType.field_223227_a_ /* OVERWORLD */) == null) {
+        if (this.minecraftClient.getIntegratedServer() == null || this.minecraftClient.getIntegratedServer().getWorld(DimensionType.OVERWORLD) == null) {
             return null;
         } else {
-            return this.minecraftClient.getIntegratedServer().getWorld(DimensionType.field_223227_a_ /* OVERWORLD */)
+            return this.minecraftClient.getIntegratedServer().getWorld(DimensionType.OVERWORLD)
                     .getPlayers((p) -> p.getName().getString().equals(this.minecraftClient.player.getName().getString()))
                     .get(0);
         }
